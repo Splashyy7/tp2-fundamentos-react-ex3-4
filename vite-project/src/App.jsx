01a25f1182381b02c4482c5963abf6d17c1cd186
João Pedro Oliveira Gritz de Oliveira <joao.poliveira@al.infnet.edu.br>
2024-09-04 07:25:15 -0300

    intial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Hello, World!</h1>
+      <p className="description">hello world</p>
+    </div>
+  );
+}
+
+export default App;
